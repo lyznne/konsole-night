2a5dc2322fb463eba1594679e3f29d7307dbf0a0
lyznne <emuthiani26@gmail.com>
2023-08-22 04:15:39 +0300

    :sparkles: navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,8 +2,20 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>
-      <h1>navbar</h1>
+    <div className="navbar">
+      <div className="nav flex">
+        <div className="logo">
+          <a href="#"></a>
+          <h1>Konsole night</h1>
+        </div>
+        <div className="nav-items flex">
+          <a className='nav-link' href="#features">Features</a>
+          <a className='nav-link' href="#support">support</a>
+          <a href="#installation">
+            <button className="btn-primary">use theme</button>
+          </a>
+        </div>
+      </div>
     </div>
   )
 }
