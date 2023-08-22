3283bdbb8b01784e43a0a6f736efbb1cf9ff01f3
lyznne <emuthiani26@gmail.com>
2023-08-22 18:20:59 +0300

    :sparkles: footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import Main from './layouts/Main';
 
 function App() {
   return (
-    <Navbar />
+    <>
+    <Navbar /> 
+    <Main />
+    </>
+    
   );
 }
 
--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -2,23 +2,28 @@ import React from 'react';
 import polygon from "../assets/polygon.svg";
 import smile from "../assets/smile.svg";
 
-
 const Footer = () => {
   return (
     <footer>
-        <h1>konsole night</h1>
+      <div className="footer ">
+        <a href="" target='_blank'><h1 className='title'>konsole night</h1>
+        </a>
         <div className="footer-items flex">
-            <p>2023 konsole night . all rights reserved</p>
-            <div className="user flex">
-                <img src={polygon} alt="" className="polygon" />
-                <p>made by -</p><a href="#">enos(lyznne)</a>
-                <img src={smile} alt="smile" />
-            </div>
-            <div className="top flex">
+          <div className="rights flex">
+            <span className='text copy'>&copy;</span>
+            <p className='text'> 2023 konsole night . all rights reserved</p>
+          </div>
+          <div className="user flex">
             <img src={polygon} alt="" className="polygon" />
-                <p>scroll top</p>
-            </div>
+            <p className='text'>  made by -</p><a href="#" target='_blank' className='text link-user'>enos(lyznne)</a>
+            <img src={smile} alt="smile" />
+          </div>
+          <div className="top flex">
+            <img src={polygon} alt="" className="polygon" />
+            <p className='text'>scroll top</p>
+          </div>
         </div>
+      </div>
     </footer>
   )
 }
